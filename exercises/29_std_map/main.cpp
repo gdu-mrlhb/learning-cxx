1712726637a41ead5f3099b394af4e2608c62125
mrlhb <steve@gdu-tech.com>
2026-01-18 09:52:57 +0800

    Finish completed 29 exercises(0~28).

--- exercises/29_std_map/main.cpp
+++ exercises/29_std_map/main.cpp
@@ -12,6 +12,7 @@ bool key_exists(std::map<k, v> const &map, k const &key) {
 template<class k, class v>
 void set(std::map<k, v> &map, k key, v value) {
     // TODO: 实现函数
+    map.push
 }
 
 // ---- 不要修改以下代码 ----
