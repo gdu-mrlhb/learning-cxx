8c3d60832669a9514c9eddc68c90641d6b838294
mrlhb <steve@gdu-tech.com>
2026-01-21 14:11:08 +0800

    Finish completed 34 exercises(0~33).

--- exercises/31_std_shared_ptr/main.cpp
+++ exercises/31_std_shared_ptr/main.cpp
@@ -10,36 +10,44 @@ int main(int argc, char **argv) {
     std::shared_ptr<int> ptrs[]{shared, shared, shared};
 
     std::weak_ptr<int> observer = shared;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 4, "");
 
     ptrs[0].reset();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     ptrs[1] = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
-    ptrs[2] = std::make_shared<int>(*shared);
-    ASSERT(observer.use_count() == ?, "");
+    ptrs[2] = std::make_shared<int>(*shared);   //指向新对象xx，引用为1；原有的ptrs[2]对应shared原始对象的引用计数减1到1
+    ASSERT(observer.use_count() == 1, "");
 
-    ptrs[0] = shared;
-    ptrs[1] = shared;
-    ptrs[2] = std::move(shared);
-    ASSERT(observer.use_count() == ?, "");
+    ptrs[0] = shared;       //2                 //shared原始对象的引用计数增1到2
+    ptrs[1] = shared;       //3                 //shared原始对象的引用计数增1到3
+    ptrs[2] = std::move(shared);                //ptrs[2]放弃原有对象xx，引用从1清零并释放内存；接管shared变量-实际对象强引用计数不增不减为3，shared变量已清空
+    ASSERT(observer.use_count() == 3, "");      
+    //ASSERT(shared.use_count() == 0, "");        //test
+    //ASSERT(shared  == nullptr, "");             //test
+    
+    std::ignore = std::move(ptrs[0]);           //？？ptrs[0]也清零，放弃指针引用，实际变量强引用计数变为：2
+    //ASSERT(observer.use_count() == 3, "");    //test---
+    //ASSERT(ptrs[0].use_count() == 3, "");     //test---
+    //ASSERT(ptrs[0].get() != nullptr, "");     //test---
+    ptrs[1] = std::move(ptrs[1]);               //首先减1清空，然后赋值增1，综合无影响
+    //ASSERT(observer.use_count() == 3, "");    //test---
+    ptrs[1] = std::move(ptrs[2]);               //ptrs[2]指针清空，强引计数减1到2；ptrs[1]指向同一对象接收控制权、不影响强计数
+    ASSERT(observer.use_count() == 2, "");
+    // ASSERT(ptrs[2].use_count() == 0, "");    //test
+    // ASSERT(ptrs[2]  == nullptr, "");         //test
 
-    std::ignore = std::move(ptrs[0]);
-    ptrs[1] = std::move(ptrs[1]);
-    ptrs[1] = std::move(ptrs[2]);
-    ASSERT(observer.use_count() == ?, "");
+    shared = observer.lock();                   //实际对象强计数增加1变为3，shared变量重新有效，共享指向一个变量
+    ASSERT(observer.use_count() == 3, "");
 
-    shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    shared = nullptr;                           //实际对象强计数减1变为2
+    for (auto &ptr : ptrs) ptr = nullptr;       //所有对象强制清空，引用计数逐次减少至清零释放内存
+    ASSERT(observer.use_count() == 0, "");
 
-    shared = nullptr;
-    for (auto &ptr : ptrs) ptr = nullptr;
-    ASSERT(observer.use_count() == ?, "");
-
-    shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    shared = observer.lock();                   //
+    ASSERT(observer.use_count() == 0, "");
 
     return 0;
 }
--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include "../exercise.h"
 #include <numeric>
+#include    <vector>
 
 // READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
-
 int main(int argc, char **argv) {
     using DataType = float;
     int shape[]{1, 3, 224, 224};
@@ -11,7 +11,9 @@ int main(int argc, char **argv) {
     //       - 形状为 shape；
     //       - 连续存储；
     //       的张量占用的字节数
-    // int size =
+    long arrsize    =   sizeof(shape)/sizeof(shape[0]);
+    std::vector<int> shapeDim(shape,shape+arrsize);
+    int size = std::accumulate(shapeDim.begin(),shapeDim.end(),sizeof(DataType),[](int a, int b) { return a * b; });
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
